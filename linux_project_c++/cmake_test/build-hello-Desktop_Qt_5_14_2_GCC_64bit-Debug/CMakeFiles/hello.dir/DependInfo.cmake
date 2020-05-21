7d89dca3c6db7849263185c8349805812b7cc7a3
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-05-21 10:44:47 +0800

    linux qtcreator cmake demo

---
+++ linux_project_c++/cmake_test/build-hello-Desktop_Qt_5_14_2_GCC_64bit-Debug/CMakeFiles/hello.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kk-1988/work/cpp_test/linux_project_c++/cmake_test/hello/main.cpp" "/home/kk-1988/work/cpp_test/linux_project_c++/cmake_test/build-hello-Desktop_Qt_5_14_2_GCC_64bit-Debug/CMakeFiles/hello.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
