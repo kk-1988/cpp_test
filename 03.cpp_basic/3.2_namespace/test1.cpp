19d9f0a5e0789648ab280bf4c4f5423d38ddc63a
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-05-23 18:09:58 +0800

    不同文件namespace的引用方法

--- 03.cpp_basic/3.2_namespace/test1.cpp
+++ 03.cpp_basic/3.2_namespace/test1.cpp
@@ -13,6 +13,9 @@ namespace NS1
 
 using NS1::func1;			//第二种namespace使用方法
 //using namespace NS1;		//第三种namespace使用方法
+namespace NS2{void func3(void);};
+//namespace NS2{extern void func3(void);};
+
 using namespace NS2;
 
 //以下main函数和上面的NS1是在两个namespace中，main函数相当于在默认的namespace中
