f8bca2ae62712994aa2b2584a930933303939b31
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-05-23 17:42:30 +0800

    命名空间demo

---
+++ 03.cpp_basic/3.2_namespace/test1.cpp
@@ -0,0 +1,26 @@
+namespace NS1
+{
+	void func2(void)
+	{
+		
+	}
+	
+	void func1(void)
+	{
+		func2();
+	}
+};
+
+using NS1::func1;			//第二种namespace使用方法
+//using namespace NS1;		//第三种namespace使用方法
+using namespace NS2;
+
+//以下main函数和上面的NS1是在两个namespace中，main函数相当于在默认的namespace中
+int main(void)
+{
+	//NS1::func1();			//第一种namespace使用方法
+	//func1();				
+	//NS1::func2();	
+	func3();
+	return 0;
+}
---
+++ 03.cpp_basic/3.2_namespace/test2.cpp
@@ -0,0 +1,10 @@
+namespace NS2
+{
+	void func3(void)
+	{
+		
+		
+	}
+
+	
+};
