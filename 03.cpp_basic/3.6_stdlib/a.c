094fda32e7338b544af5edf478de18f04b578652
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-05-29 07:26:03 +0800

    cplusplus宏定义使用

--- 03.cpp_basic/3.6_stdlib/a.c
+++ 03.cpp_basic/3.6_stdlib/a.c
@@ -1,7 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+
+#ifdef __cplusplus
+#define USECPP 1
+#else
+#define USECPP 0
+#endif
 
 int main(void)
 {
-	printf("hello world!!\r\n");
+	//c程序时没有定义，c++编译时则有定义
+	printf("hello c file %d\r\n",USECPP);
 	return 0;
 }
