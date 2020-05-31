6860dd5260e54d3e36534d081f5eb500051b2ef4
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-06-01 07:34:16 +0800

    其他进制测试

--- 03.cpp_basic/3.7_cout/cout.cpp
+++ 03.cpp_basic/3.7_cout/cout.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(void)
 {
-	int val = 8;
+	int val = 18,val2 = 19;
 	
-	cout << "hello cpp." << val << endl;
+	cout << "hello cpp." << "0x" << hex << val << "--" << dec << val2 << endl;
 	return 0;
 }
