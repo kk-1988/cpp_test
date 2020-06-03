fe40f2daa71e6e2c52b05f627e038a5229a06387
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-06-04 07:33:02 +0800

    使用浮点位数标识符进行测试

--- 03.cpp_basic/3.7_cout/cout.cpp
+++ 03.cpp_basic/3.7_cout/cout.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 int main(void)
 {
-	int val = 18,val2 = 19;
+	//int val = 18,val2 = 19;
 	
-	cout << "hello cpp." << "0x" << hex << val << "--" << dec << val2 << endl;
+	//cout << "hello cpp." << "0x" << hex << val << "--" << dec << val2 << endl;
+	
+	double a = 1234.56789;
+	
+	//setprecision 用来表示占用了多少位
+	cout << setprecision(9) << a << endl;
 	
 	return 0;
 }
