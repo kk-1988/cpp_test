ac9bcb80b439c7b88e22acbf22eeee1e6eec2cac
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-06-09 07:23:11 +0800

    cin 程序demo

---
+++ 03.cpp_basic/3.8_cin/cintest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int val;
+	
+	cout << "please input a num:" << endl;
+	cin >> hex >> val;
+	cout << "the number is :" << val << endl;
+	
+	return 0;
+}
