f2aeb75790c7c7df7dd47c8be5877750c2c80cd7
xiaobo.kong <xiaobo.kong@aliyun.com>
2021-05-13 08:08:04 +0800

    cout 使用demo

--- 03.cpp_basic/3.4_anonymous_namespace/hello.cpp
+++ 03.cpp_basic/3.4_anonymous_namespace/hello.cpp
@@ -1,25 +1,9 @@
 #include <iostream>
 using namespace std;
 
-//匿名命名空间声明
-namespace
-{
-	extern void func2(void);	
-}
-
-namespace NS
-{
-	void func1(void)
-	{
-		//cout << "func1 from namespace NS" << endl;
-		func2();
-	}
-};
-
 int main(void)
 {
-	cout << "hello world!" << endl;
-	NS::func1();
-	
+	cout << "hello cpp world!!!" << endl;
+	//std::cout << "hello cpp world!!!!" << std::endl;
 	return 0;
 }
