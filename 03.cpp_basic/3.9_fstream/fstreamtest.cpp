34ce9fe0ce418e8895fa8b0053bc99a71a291f48
kongxiaobo <xiaobo.kong@aliyun.com>
2021-12-13 18:53:24 +0800

    cpp fstream demo

---
+++ 03.cpp_basic/3.9_fstream/fstreamtest.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char *argv[])
+{
+    fstream fs;
+    fs.open("");
+
+    cout << "hello world,c++" << endl;
+    return 0;
+}
